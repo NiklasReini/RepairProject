4d6ec7e1ee48488488f256f7d1983f206e3db17d
Niklas Reini <niklas.reini01@gmail.com>
2020-12-02 16:00:56 +0200

    commit

--- src/main/java/com/example/RepairProject/domain/Repair.java
+++ src/main/java/com/example/RepairProject/domain/Repair.java
@@ -1,9 +1,14 @@
 package com.example.RepairProject.domain;
 
+import java.awt.Image;
+
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import org.springframework.web.multipart.MultipartFile;
 
 @Entity
 public class Repair {
@@ -12,14 +17,16 @@ public class Repair {
 	private long id;
 	private String title, description;
 	private int price;
+	private MultipartFile image;
 	
 	public Repair() {}
 	
-	public Repair(String title, String description, int price) {
+	public Repair(String title, String description, int price, MultipartFile image) {
 		super();
 		this.title = title;
 		this.description = description;
 		this.price = price;
+		this.image = image;
 	}
 	public long getId() {
 		return id;
@@ -45,6 +52,15 @@ public class Repair {
 	public void setPrice(int price) {
 		this.price = price;
 	}
+
+	public MultipartFile getImage() {
+		return image;
+	}
+
+	public void setImage(MultipartFile image) {
+		this.image = image;
+	}
+	
 	
 	
 }
--- src/main/java/com/example/RepairProject/web/RepairController.java
+++ src/main/java/com/example/RepairProject/web/RepairController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.example.RepairProject.domain.Repair;
 import com.example.RepairProject.domain.RepairRepository;
 
@@ -48,4 +51,5 @@ public class RepairController {
 		return "add";
 		
 	}
+
 }
