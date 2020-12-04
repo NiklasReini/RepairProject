bdfc96535c9c8060859c15aff2628041c92d740c
Niklas Reini <niklas.reini01@gmail.com>
2020-12-04 13:53:26 +0200

    commit

--- src/main/java/com/example/RepairProject/web/UserService.java
+++ src/main/java/com/example/RepairProject/web/UserService.java
@@ -1,5 +1,34 @@
 package com.example.RepairProject.web;
 
-public class UserService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import com.example.RepairProject.domain.User;
+import com.example.RepairProject.domain.UserRepository;
+
+
+/**
+ * This class is used by spring security to authenticate and authorize user
+ **/
+@Service
+public class UserService implements UserDetailsService  {
+	private final UserRepository repository;
+
+	@Autowired
+	public UserService(UserRepository userRepository) {
+		this.repository = userRepository;
+	}
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
+    {   
+    	User curruser = repository.findByUsername(username);
+        UserDetails user = new org.springframework.security.core.userdetails.User(username, curruser.getPasswordHash(), 
+        		AuthorityUtils.createAuthorityList(curruser.getRole()));
+        return user;
+    }   
 }
