a430f2eb9e2da6d67ddf733aee04b0f69b798a61
Niklas Reini <niklas.reini01@gmail.com>
2020-12-04 13:32:55 +0200

    commit

--- src/main/java/com/example/RepairProject/RepairProjectApplication.java
+++ src/main/java/com/example/RepairProject/RepairProjectApplication.java
@@ -1,10 +1,13 @@
 package com.example.RepairProject;
 
 import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.example.RepairProject.domain.User;
+import com.example.RepairProject.domain.UserRepository;
 import com.example.RepairProject.domain.Repair;
 import com.example.RepairProject.domain.RepairRepository;
 
@@ -16,9 +19,14 @@ public class RepairProjectApplication {
 	}
 
 	@Bean
-	public CommandLineRunner demo(RepairRepository repository) {
+	public CommandLineRunner demo(RepairRepository repository, UserRepository userRepository) {
 		return(args) -> {
-
+			
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+			userRepository.save(user1);
+			userRepository.save(user2);
+			
 		};
 	}
 	
