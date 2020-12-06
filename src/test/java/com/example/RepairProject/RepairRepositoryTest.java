58e6f322bc4772fbbff29703f3548110251b3e7b
Niklas Reini <niklas.reini01@gmail.com>
2020-12-06 11:27:16 +0200

    commit

--- src/test/java/com/example/RepairProject/RepairRepositoryTest.java
+++ src/test/java/com/example/RepairProject/RepairRepositoryTest.java
@@ -8,7 +8,6 @@ import java.util.Date;
 import java.util.List;
 
 import org.junit.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
